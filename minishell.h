45e8d51ebd33bf425ab7601ba90b73e92957b6c6
jerryhp <horjarry@gmail.com>
2025-09-10 22:57:05 +0800

    TODO: work on splitting and grouping

--- minishell.h
+++ minishell.h
@@ -6,35 +6,49 @@
 /*   By: jhor <jhor@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 18:01:03 by jhor              #+#    #+#             */
-/*   Updated: 2025/09/08 21:01:21 by jhor             ###   ########.fr       */
+/*   Updated: 2025/09/09 15:43:06 by jhor             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include "Libft/libft.h"
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <stdlib.h>
 
-enum	e_token
+enum	e_token //!lexical grouping
 {
-	word,
-	operator,
-	
+	WORD,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	APPEND,
+	HEREDOC	
 };
+
+typedef struct s_word
+{
+	void	*cmd;
+	void	*args;
+	void	*filename;
+}t_word;
 
 typedef struct s_union
 {
-	
+	t_word	*word;
 }t_union;
 
-typedef struct s_cmd
+typedef struct s_pipe
 {
-	char	*cmd;
-	char	*args;
-	char	*redir_op;
-}t_cmd;
+	t_union	*linker;
+}t_pipe;
+
+// typedef struct s_operator
+// {
+	
+// }t_operator;
 
 #endif
--- src/readline.c
+++ src/readline.c
@@ -6,13 +6,119 @@
 /*   By: jhor <jhor@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 19:00:15 by jhor              #+#    #+#             */
-/*   Updated: 2025/09/08 20:57:27 by jhor             ###   ########.fr       */
+/*   Updated: 2025/09/10 21:26:38 by jhor             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+void	word_group(char **tokens, char *result)
+{
+	int	i;
+	int	j;
+	int	quotes;
 
+	i = 0;
+	j = 0;
+	while (tokens[j])
+	{
+		while (result[i])
+		{
+			quotes = 0;
+			if ((result[i] >= 'A' && result[i] <= 'Z') ||
+				(result[i] >= 'a' && result[i] <= 'z') || result[i] == '.')
+				tokens[j] = result[i];
+			else if ((result[i] == 34 || result[i] == 39) && quotes == 1)
+			{
+				tokens[j] = result[i];
+				quotes = 0;
+			}
+			else if ((result[i] == 34 || result[i] == 39) && quotes == 0)
+			{
+				tokens[j] = result[i];
+				quotes = 1;
+			}
+			else if (result[i] == ' ' && quotes != 1)
+				break;
+			i++;
+		}
+		j++;
+	}
+}
+
+char **token_split(char *result, char **tokens) //!Could change this function for other purposes
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (result[i])
+	{
+		if ((result[i] >= 'A' && result[i] <= 'Z') || 
+			(result[i] >= 'a' && result[i] <= 'z') || 
+			result[i] == 34 || result[i] == 39)
+				word_group(&tokens, result);
+		i++;
+	}
+}
+
+if (result is queal to character "" or '', call function to handle count for quoted strings)
+if (result is equal to character a to z plus '.' and the next is a space, add one count)
+if (result is equal to character > >> < << | all add on count)
+
+
+
+int	token_count(char *result)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (result[i])
+	{
+		if (result[i] == 34 || result[i] == 39)
+			count_quote(&count);
+		if ((result[i] >= 'A' && result[i] <= 'Z') ||
+			(result[i] >= 'a' && result[i] <= 'z') || 
+			result[i] == '.' && (result[i] != 34 || result[i] =!= 39) 
+			&& result[i + 1] == ' ')
+			count++;
+		if (result[i] == '>' || result[i] == '<' || result[i] == '|')
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+char **ft_tokenize(char *result)
+{
+	char	**tokens;
+	int		length;
+	length = ft_strlen(result);
+	//!how do i determine the length of the array ?
+	token_split(&tokens, result);
+	if (!tokens)
+	{
+		ft_putstr_fd("Fail to tokenize", 2);
+		exit (127);
+	}
+	return (tokens);
+}
+
+void	free_tokens(char **tokens)
+{
+	int	i;
+
+	i = 0;
+	while (tokens[i])
+	{
+		free(tokens[i]);
+		i++;
+	}
+	free(tokens);
+}
 
 int main(int argc, char **argv)
 {
@@ -20,14 +126,24 @@ int main(int argc, char **argv)
 	// 	return (1);
 	(void)argv;
 	(void)argc;
-	char *result = NULL;
 	
 	while (1)
 	{
+		char *result = NULL;
 		result = readline("minishell$ ");
 		if (!result)
-			break;
+		break;
 		add_history(result);
+		// char **tokens = NULL;
+		// tokens = ft_tokenize(result);
+		// int i = 0;
+		// while (tokens[i])
+		// {
+		// 	printf("%s\n", tokens[i]);
+		// 	i++;
+		// }
+		// free_tokens(tokens);
+		printf("%s\n", result);
 		free(result);
 	}
 	return (0);
