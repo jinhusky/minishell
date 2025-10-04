641746ab5e5a61b44b4eb0027c7f1f5aa0e5b585
Jerry Hor <jhor@u82z05s02.42kl.edu.my>
2025-10-04 18:37:10 +0800

    TODO: work on attach_treenode & treenode assignment

--- src/ast/parse_pipe.c
+++ src/ast/parse_pipe.c
@@ -6,7 +6,7 @@
 /*   By: jhor <jhor@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 16:13:33 by jhor              #+#    #+#             */
-/*   Updated: 2025/10/01 17:54:44 by jhor             ###   ########.fr       */
+/*   Updated: 2025/10/04 18:35:55 by jhor             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,18 +39,49 @@ t_token	*token_peek(t_parser *p)
 	return (p->cursor);
 }
 
-t_ast	*create_treenode(t_ast *temp)
+t_ast	*create_treenode(t_ast *treenode)
 {
-	temp = malloc(sizeof(t_ast));
-	if (!temp)
+	treenode = malloc(sizeof(t_ast));
+	if (!treenode)
 		return (NULL);
-	temp->children = NULL;
-	temp->childcount = 0;
-	temp->token_ref = NULL;
-	temp->type = 0;
-	return (temp);
+	treenode->children = NULL;
+	treenode->childcount = 0;
+	treenode->token_ref = NULL;
+	treenode->type = 0;
+	return (treenode);
 }
-t_ast	*parse_command(t_ast *node, t_parser *p); //sees token is a WORD, malloc AST_COMMAND && AST_WORD
+
+void	assign_treenode(t_ast *branch, t_parser *p) //potentially not needed
+{
+	if (token_peek(p)->token == WORD && branch->childcount == 0)
+		branch->type = AST_WORD;
+	else if (token_peek(p)->token == WORD && branch->childcount > 0)
+		branch->type = AST_COMMAND;
+	return;
+}
+
+void	attach_treenode(t_ast *branch, t_ast *leaf) //continue here
+{
+	if (!branch || !leaf)
+		return;
+	branch->children = malloc(sizeof(char *) * 1);
+	if (!branch->children)
+		return;
+	while (*(branch->children))
+	{
+		
+	}
+}
+
+t_ast	*parse_command_word(t_ast *branch, t_parser *p)
+{
+	branch->type = AST_WORD;
+	branch->children = malloc(sizeof(char *) * 1);
+	if (!branch->children)
+		return (NULL);
+	//assign the token lexeme into children array
+	return (branch);
+}
 
 t_ast	*parse_component(t_ast *node, t_parser *p)
 {
@@ -83,38 +114,52 @@ t_ast	*parse_components(t_ast *node, t_parser *p)
 	return (node);
 }
 
-t_ast	*parse_simple_command(t_ast *node, t_parser *p)
+t_ast	*parse_simple_command(t_ast *branch, t_parser *p)
 {
-	if (token_peek(p)->token == WORD && check_children(node) == 0) //check whether there are existing AST_COMMAND
-		node = parsing_command(); // create a fresh branch AST_COMMAND && AST_WORD following command_word -> word
-	else if (token_peek(p)->token == WORD && check_children(node) == 1) //existing AST_COMMAND branch
-		node = parsing_components(); //create AST_WORD then attach to existing AST_COMMAND
-	p->cursor = get_token(p);
-	return (node);
-	
+	t_ast	*command;
+
+	command = NULL;
+	if (token_peek(p)->token == WORD)
+	{
+		command = create_treenode(command);
+		command = parse_command_word(command, p); // create a fresh branch AST_COMMAND && AST_WORD following command_word -> word
+		attach_treenode(branch, command);
+	}
+	return (branch);
+	// p->cursor = get_token(p);
+	//else if (token_peek(p)->token == WORD) //existing AST_COMMAND branch
+	//	node = parse_components(); //create AST_WORD then attach to existing AST_COMMAND
 }
 
-t_ast	*parse_pipeline(t_ast *node, t_parser *p)
+t_ast	*parse_pipeline(t_ast *root, t_parser *p)
 {
+	t_ast	*branch;
+
+	branch = NULL;
+	root = create_treenode(root);
+	if (token_peek(p)->token == WORD)
+	{
+		branch = create_treenode(branch);
+		branch = parse_simple_command(branch, p);
+		attach_treenode(root, branch);
+	}
+	p = get_token(p);
+	return (root);
 	//if token_peek is WORD then call parse_simple_command;
 	//get_token to get the next token;
 	//if token_peek is REDIRECTION then call parse_simple_command;
 	//if token_peek is PIPE then get_token to consume and call parse_simple_command
-	if (token_peek(p)->token == WORD)
-		parse_simple_command(node, p);
 	// else if (token_peek(p)->token == REDIR_IN || 
 	// 	token_peek(p)->token == REDIR_OUT ||
 	// 	token_peek(p)->token == HEREDOC || token_peek(p)->token == APPEND)
 	// 	parse_simple_command();
-	p->cursor = get_token(p);
 	// if (token_peek(p)->token == PIPE)
 	// {
 	// 	p->cursor = get_token(p);
 	// 	parse_simple_command();
 	// }
-	if (!token_peek(p))
+	// if (!token_peek(p))
 		// error_handle;
-	return (node);
 }
 
 void	parsing(t_ast *node, t_token *token, t_parser *p)
