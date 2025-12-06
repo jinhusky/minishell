48678e5bcdea100a87aa6ed6ef77f7489b5d4b1b
kationg <tkjprof@gmail.com>
2025-12-06 23:36:22 +0800

    taking files from other branch2

--- src/execution/build_pipeline.c
+++ src/execution/build_pipeline.c
@@ -6,29 +6,101 @@
 /*   By: kationg <kationg@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 23:09:42 by kationg           #+#    #+#             */
-/*   Updated: 2025/12/06 23:27:43 by kationg          ###   ########.fr       */
+/*   Updated: 2025/12/06 23:35:46 by kationg          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-void    walk_ast(t_node *node, )
+
+static int	count_argc(t_ast *cmd)
 {
-    int i;
-    t_node  *ptr;
+	int		count;
+	int		i;
+	t_ast	*ptr;
+	
+	count = 0;
+	i = 0;
+	while (i < cmd->childcount)
+	{
+		ptr = cmd->children[i];
+		if (ptr->type == AST_WORD || ptr->type == AST_ARGUMENT)
+			count++;
+		i++;
 
-    i = 0;
-    while (i < node->childcount)
-    {
-        ptr = node->children[i];
-        walk_ast(ptr);
-        i++;
-    }
+	}
+	return (count);
 }
-void    build_pipeline(t_node *root, t_globe *p)
+
+char **build_argv_array(t_ast *cmd)
 {
-    int argc;
-
-    argc = count_argc(root);
-
+	int		i;
+	t_ast	*child;
+	char	**argv;
+	
+	if (!cmd || cmd->type != AST_COMMAND)
+		return (NULL);
+	argv = ft_calloc(count_argc(cmd) + 1, sizeof(char *));
+	i = 0;
+	while (i < node->childcount && node->children[i])
+	{
+		if (node->children[i]->type == AST_WORD)
+			str = node->children[i]->token_ref->lexeme;
+		else if (node->children[i]->type == AST_ARGUMENT)
+			str = node->children[i]->children[0]->token_ref->lexeme;
+		argv[i] = ft_strdup(str);
+		i++;
+	}
+	return (argv);
 }
+
+char **build_envp_array(t_shell shell)
+{
+	t_envp	*ptr;
+	int	i;
+	
+	i = 0;
+	ptr = shell.envp_head;
+	char **res;
+	res = ft_calloc(shell.size + 1, sizeof(char *));
+	while (ptr)
+	{
+		res[i] = ft_strdup(ptr->key);
+		res[i] = ft_strjoin(res[i], "=");
+		res[i] = ft_strjoin(res[i], ptr->value);
+		ptr = ptr->next;
+		i++;
+	}
+	return res;
+}
+
+void execute(char **envp_exec, char **argv, t_shell *shell)
+{
+	char	*path_env;
+	char	**paths;
+	int		i;
+	char	*fullpath;
+
+	i = 0;
+	if (ft_strchr(argv[0], '/'))
+		execve(argv[0], argv, envp_exec);
+	else
+	{
+		path_env = envp_value("PATH", NULL, *shell);
+		paths = ft_split(path_env, ':');
+		while (paths[i])
+		{
+			fullpath = ft_strjoin(paths[i], "/");
+			fullpath = ft_strjoin(fullpath, argv[0]);
+			if (access(fullpath, F_OK) == 0)
+			{
+				if (execve(fullpath, argv, envp_exec) == -1)
+					perror("Command not found");
+				break;
+			}
+			i++;
+		}
+	}
+}
+
+
